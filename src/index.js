7558839c65eccf017008dca3e829df60c26eec5d
DoraPicPay <isadora.jardim@picpay.com>
2025-03-13 15:22:54 -0300

    Arrumando index

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from "./components/Apresentations/InitialPage/App.js"
 import './index.css';
-import GameScreen from './components/GameScreen/GameScreen.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GameScreen />
+    <App />
   </React.StrictMode>
 );
